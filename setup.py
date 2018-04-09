f186f35f9a0c5a6594882ff319f10045f19fc4a7
Audric Schiltknecht <audric.schiltknecht@viagenie.ca>
2018-04-09 15:20:24 -0400

    Remove old licence

--- setup.py
+++ setup.py
@@ -1,21 +1,5 @@
 #!/bin/env python2
 # -*- coding: utf-8 -*-
-# Copyright (c) 2015, Viagenie inc.
-# All rights reserved.
-
-# Redistribution and use in source and binary forms, with or without
-# modification, is not permitted.
-
-# This software is provided ``as is'' and any
-# Express or implied warranties, including, but not limited to, the implied
-# Warranties of merchantability and fitness for a particular purpose are
-# Disclaimed. In no event shall the regents and contributors be liable for any
-# Direct, indirect, incidental, special, exemplary, or consequential damages
-# (including, but not limited to, procurement of substitute goods or services;
-# Loss of use, data, or profits; or business interruption) however caused and on
-# Any theory of liability, whether in contract, strict liability, or tort
-# (including negligence or otherwise) arising in any way out of the use of this
-# Software, even if advised of the possibility of such damage.
 from setuptools import setup, find_packages
 
 setup(
